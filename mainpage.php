65591fea118d3ea7116bc155f857d8fd42659552
DimaOffice <polkovnikovd46@gmail.com>
2024-02-29 13:21:30 +0300

    feat:Добавленна mainpage c кнопками регистрации и авторизации

---
+++ mainpage.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'vendor/autoload.php';
+?>
+<html>
+<div>
+	<form class="reg-button" action="registration.php">
+		<button type="submit" name="registration-button" >Registration</button>
+	</form>
+	<form class="auth-button">
+		<button type="submit" name="authorization-button" >Authorization</button>
+	</form>
+</div>
+
+
+</html>
